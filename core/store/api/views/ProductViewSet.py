955359b7c41e986a88a3da3544b9f3d22c61479d
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-08 20:20:20 +0300

    TokenAuthentication has been assigned to the products endpoint.

--- core/store/api/views/ProductViewSet.py
+++ core/store/api/views/ProductViewSet.py
@@ -2,10 +2,12 @@ from rest_framework import viewsets
 from store.models import Product
 from store.api.serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class ProductViewSet(viewsets.ModelViewSet):
 
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication,]
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
