a77bbda6dfb3ee4a949bdbb552f6a54eb843d92b
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-08 15:52:45 +0300

    The models were defined within the __init__ file.

---
+++ core/store/models/__init__.py
@@ -0,0 +1,7 @@
+from store.models.Cart import Cart
+from store.models.CartItem import CartItem
+from store.models.Category import Category
+from store.models.Order import Order
+from store.models.OrderItem import OrderItem
+from store.models.Product import Product
+from store.models.UserProfile import UserProfile
