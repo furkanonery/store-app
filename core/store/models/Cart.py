50629959af30be654c8f63c92cf60bfc45807888
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-08 15:49:16 +0300

    The Cart model has been created.

---
+++ core/store/models/Cart.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+from store.models import Product
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product", through='CartItem')
