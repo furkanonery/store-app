42f1a8c04a126bd9b430beee1e8231c31c71ef9b
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-08 15:50:09 +0300

    The Order model has been created.

---
+++ core/store/models/Order.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from store.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField('Product', through='OrderItem')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
