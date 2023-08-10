004ccc1c266572c61db6194488f3b10084ff4fa6
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-11 00:47:44 +0300

    CartViewSet has been created.

---
+++ core/store/api/views/CartViewSet.py
@@ -0,0 +1,19 @@
+from rest_framework.viewsets import GenericViewSet
+from store.models import Cart
+from store.api.serializers import CartSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from store.api.permissions import OwnYourCarts
+from rest_framework import mixins
+
+class CartViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   GenericViewSet):
+
+    permission_classes = [IsAuthenticated, OwnYourCarts]
+    authentication_classes = [TokenAuthentication,]
+
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
--- core/store/api/views/__init__.py
+++ core/store/api/views/__init__.py
@@ -1,1 +1,2 @@
 from store.api.views.ProductViewSet import ProductViewSet
+from store.api.views.CartViewSet import CartViewSet
