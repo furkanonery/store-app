88a90c2d3e399a22aee8690423a656fc2ef3b551
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-11 00:48:30 +0300

    CartViewSet has been connected to the carts endpoint.

--- core/store/api/urls.py
+++ core/store/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from store.api.views import ProductViewSet
+from store.api.views import ProductViewSet, CartViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'carts', CartViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
