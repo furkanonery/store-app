75a430670e3c2cb571ae328cd04f4cd43a9c0e18
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-16 12:38:42 +0300

    "Product" was given as the basename for the product API.

--- core/store/api/urls.py
+++ core/store/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from store.api.views import ProductViewSet, CartViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
+router.register(r'products', ProductViewSet, basename='Product')
 router.register(r'carts', CartViewSet)
 
 urlpatterns = [
