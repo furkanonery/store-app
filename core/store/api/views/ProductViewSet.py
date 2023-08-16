492530e11c33f3971de9ea9e20a145d452f0fc84
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-16 12:37:55 +0300

    A query parameter feature has been added to the product API.

--- core/store/api/views/ProductViewSet.py
+++ core/store/api/views/ProductViewSet.py
@@ -6,8 +6,18 @@ from rest_framework.authentication import TokenAuthentication
 
 class ProductViewSet(viewsets.ModelViewSet):
 
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication,]
-
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+    
+        category = self.request.query_params.get('category')
+
+        if category:
+            queryset = Product.objects.filter(category__name=category)
+            return queryset
+        queryset = Product.objects.all()
+        return queryset
+
+        
