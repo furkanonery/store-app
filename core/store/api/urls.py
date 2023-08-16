0439406581c21a5d746efb75be8d591ea08eec9b
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-16 20:21:20 +0300

    The "orders" endpoint is registered.

--- core/store/api/urls.py
+++ core/store/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from store.api.views import ProductViewSet, CartViewSet
+from store.api.views import ProductViewSet, CartViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='Product')
-router.register(r'carts', CartViewSet)
+router.register(r'carts', CartViewSet, basename='Cart')
+router.register(r'orders', OrderViewSet, basename='Order')
 
 urlpatterns = [
     path('', include(router.urls)),
