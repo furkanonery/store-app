56e153105d70c198e5260a0122511c7835832a0f
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-17 16:29:19 +0300

    Added str method to Product model."

--- core/store/models/Product.py
+++ core/store/models/Product.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.category}:{self.name}"
