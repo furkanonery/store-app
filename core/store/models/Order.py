a3c1887ca2596a604551210efd9468baf81ea342
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-17 16:29:07 +0300

    Added str method to Order an OrderItem model."

--- core/store/models/Order.py
+++ core/store/models/Order.py
@@ -7,3 +7,6 @@ class Order(models.Model):
     products = models.ManyToManyField('Product', through='OrderItem')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s order."
--- core/store/models/OrderItem.py
+++ core/store/models/OrderItem.py
@@ -5,3 +5,6 @@ class OrderItem(models.Model):
     order = models.ForeignKey("Order", on_delete=models.CASCADE)
     product = models.ForeignKey("Product", on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.order} {self.product} quantity:{self.quantity}"
