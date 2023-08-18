bcd0249c03645471e78a8122f75489cae9fabd2a
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-18 15:06:30 +0300

    ProductSerializer is simplified.

--- core/store/api/serializers/ProductSerializer.py
+++ core/store/api/serializers/ProductSerializer.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from store.models import Product
 class ProductSerializer(serializers.ModelSerializer):
 
-    category = serializers.StringRelatedField()
-
     class Meta:
         model = Product
         fields = '__all__'
