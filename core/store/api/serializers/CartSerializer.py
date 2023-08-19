8af2171c215674256c22bfb0d3cbcfd53d349b6d
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-19 18:45:47 +0300

    Updates to serializers for the CartSerializer.

--- core/store/api/serializers/CartSerializer.py
+++ core/store/api/serializers/CartSerializer.py
@@ -3,14 +3,17 @@ from store.models import Cart, CartItem
 from store.api.serializers import ProductSerializer
 
 class CartItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CartItem
-        fields = ('product', 'quantity')
+        fields = '__all__'
 
 
 class CartSerializer(serializers.ModelSerializer):
 
-    cart_items = CartItemSerializer(many=True, read_only=True)
+    cart_items = CartItemSerializer(source='cartitem_set', many=True)
+    user = serializers.StringRelatedField()
+    products = serializers.PrimaryKeyRelatedField(source='products.all', many=True, read_only=True)
 
     class Meta:
         model = Cart
