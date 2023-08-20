2179621f8d14c23785089827295b365fa7902437
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-20 14:05:36 +0300

    CartViewSet and CarSerializer is simplified.

--- core/store/api/serializers/CartSerializer.py
+++ core/store/api/serializers/CartSerializer.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
 from store.models import Cart, CartItem
 from store.api.serializers import ProductSerializer
-
+from rest_framework.fields import CurrentUserDefault
 
 class CartItemSerializer(serializers.ModelSerializer):
+    cart = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = CartItem
         fields = "__all__"
 
 
 class CartSerializer(serializers.ModelSerializer):
-    cart_items = CartItemSerializer(source="cartitem_set", many=True)
-    user = serializers.StringRelatedField()
+    cart_items = CartItemSerializer(source="cartitem_set", many=True, read_only=True)
     products = ProductSerializer(many=True, read_only=True)
+    user = serializers.HiddenField(default=CurrentUserDefault())
 
     class Meta:
         model = Cart
--- core/store/api/views/CartViewSet.py
+++ core/store/api/views/CartViewSet.py
@@ -23,15 +23,4 @@ class CartViewSet(mixins.CreateModelMixin,
         return queryset
     
     serializer_class = CartSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        if self.request.user.id == request.data['user']:
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        else:
-            headers = self.get_success_headers(serializer.data)
-            return Response({"Detail": "Authentication credentials were not provided."}, status=status.HTTP_403_FORBIDDEN, headers=headers)
+        
