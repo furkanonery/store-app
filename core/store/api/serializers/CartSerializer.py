3c9d4b97956a7c9e1dc74d3c43270335b4594954
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-20 12:55:02 +0300

    The products in the cart have been listed in detail.

--- core/store/api/serializers/CartSerializer.py
+++ core/store/api/serializers/CartSerializer.py
@@ -2,19 +2,18 @@ from rest_framework import serializers
 from store.models import Cart, CartItem
 from store.api.serializers import ProductSerializer
 
-class CartItemSerializer(serializers.ModelSerializer):
 
+class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CartSerializer(serializers.ModelSerializer):
-
-    cart_items = CartItemSerializer(source='cartitem_set', many=True)
+    cart_items = CartItemSerializer(source="cartitem_set", many=True)
     user = serializers.StringRelatedField()
-    products = serializers.PrimaryKeyRelatedField(source='products.all', many=True, read_only=True)
+    products = ProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Cart
-        fields = '__all__'
+        fields = "__all__"
