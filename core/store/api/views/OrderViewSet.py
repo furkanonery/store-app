2698f86838afbf316981b0b6c8e275170af2d8b9
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-21 16:56:56 +0300

    The OrderViewSet has been modified according to business rules.

--- core/store/api/views/OrderViewSet.py
+++ core/store/api/views/OrderViewSet.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
-from store.models import Order
+from store.models import Order, OrderItem, Cart, CartItem
 from store.api.serializers import OrderSerializer
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from store.api.permissions import OwnYourCartsOrOrder
+from rest_framework import status
+from rest_framework.response import Response
+from decimal import Decimal
 
 class OrderViewSet(mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
@@ -23,3 +26,29 @@ class OrderViewSet(mixins.CreateModelMixin,
         return queryset
 
     serializer_class = OrderSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        response = OrderSerializer(Order.objects.last())
+        return Response(response.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+
+        cart_items = CartItem.objects.filter(cart=Cart.objects.get(user=self.request.user))
+
+        order = Order.objects.create(user=self.request.user)
+
+        total_price = Decimal(0.00)
+
+        for item in cart_items:
+            OrderItem.objects.create(order = order, product = item.product, quantity = item.quantity)
+            total_price += item.quantity * item.product.price
+
+        order.total_price = total_price
+        order.save()
+
+        
