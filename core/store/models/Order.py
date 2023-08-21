6d2bcfcda0d7383d09827bff050dbafe5864aa5b
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-08-21 16:55:33 +0300

    Order model updated.

--- core/store/models/Order.py
+++ core/store/models/Order.py
@@ -5,8 +5,8 @@ from store.models import Product
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField('Product', through='OrderItem')
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default="0.00")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}'s order."
+        return f"{self.user.username}'s order. [{self.id}]"
