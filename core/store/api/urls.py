119c96b7837c7b325145f252a1b318360469ec5e
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-03 07:01:32 +0300

    The 'categories' endpoint is created.

--- core/store/api/urls.py
+++ core/store/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from store.api.views import ProductViewSet, CartViewSet, OrderViewSet, CartItemViewSet
+from store.api.views import ProductViewSet, CartViewSet, OrderViewSet, CartItemViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='Product')
 router.register(r'carts', CartViewSet, basename='Cart')
 router.register(r'orders', OrderViewSet, basename='Order')
 router.register(r'cartitems', CartItemViewSet, basename='CartItem')
+router.register(r'categories', CategoryViewSet, basename='Category')
 
 urlpatterns = [
     path('', include(router.urls)),
