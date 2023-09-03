9905d093b778531287e6a5ce1690acadd8b86c76
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-04 02:13:05 +0300

    The 'register' endpoint is created.

--- core/user/urls.py
+++ core/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from user.views import CustomAuthToken, LogoutView
+from user.views import CustomAuthToken, LogoutView, RegisterView
 
 urlpatterns = [
     path('login/', CustomAuthToken.as_view(), name='custom-login'),
-    path('logout/', LogoutView.as_view(), name='custom-logout')
+    path('logout/', LogoutView.as_view(), name='custom-logout'),
+    path('register/', RegisterView.as_view(), name='custom-register')
 ]
