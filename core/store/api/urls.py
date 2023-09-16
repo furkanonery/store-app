d416172267813113c531f16d5ba4010a17334765
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-09-16 15:30:55 +0300

    The 'userprofile' endpoint is created.

--- core/store/api/urls.py
+++ core/store/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from store.api.views import ProductViewSet, CartViewSet, OrderViewSet, CartItemViewSet, CategoryViewSet, ProductSearchViewSet
+from store.api.views import (ProductViewSet, 
+                             CartViewSet, 
+                             OrderViewSet, 
+                             CartItemViewSet, 
+                             CategoryViewSet, 
+                             ProductSearchViewSet,
+                             UserProfileViewSet,)
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='Product')
@@ -12,4 +18,5 @@ router.register(r'search', ProductSearchViewSet, basename='Search')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('userprofile/', UserProfileViewSet.as_view(), name='userprofile-update'),
 ]
